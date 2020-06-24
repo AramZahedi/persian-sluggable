16af3247c6d6e9b78fd898d27609c37354fb3699
AramZahedi <aram1376@yahoo.com>
2020-06-24 22:44:37 +0430

    Updated slug function

--- src/Persian.php
+++ src/Persian.php
@@ -8,10 +8,9 @@ class Persian
      * convert persian and arabic numbers to english number
      *
      * @param $string
-     * @param bool $format_numeber
      * @return mixed
      */
-    public static function convertNumbersToEnglish($string, $format_numeber = false)
+    public static function convertNumbersToEnglish($string)
     {
         $persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
         $arabic = ['٩', '٨', '٧', '٦', '٥', '٤', '٣', '٢', '١', '٠'];
@@ -20,10 +19,27 @@ class Persian
         $convertedPersianNums = str_replace($persian, $num, $string);
         $englishNumbersOnly = str_replace($arabic, $num, $convertedPersianNums);
 
-        if ($format_numeber) {
-            return number_format($englishNumbersOnly);
-        }
         return $englishNumbersOnly;
+    }
+
+    /**
+     * convert persian and arabic numbers to english number
+     *
+     * @param $string
+     * @return mixed
+     */
+    public static function convertNumbersToPersian($string)
+    {
+        $persian = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+        $english = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+        $result = self::convertNumbersToEnglish($string);
+
+        foreach ($english as $index => $num) {
+            $result = str_replace($num, $persian[$index], $result);
+        }
+
+        return $result;
     }
 
     /**
